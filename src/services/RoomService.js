35c7a2dc893f369f0dc6a1fbadc7e9ca51f9f884
linuxenthusiastic <mbitakrec@gmail.com>
2026-06-19 22:48:43 -0400

    feat: [HU-05] implementar validación de rango de fechas en RoomService.availableRooms

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -1,40 +1,43 @@
-import { getRoomStrategy } from '../models/RoomStrategy.js'
+import { getRoomStrategy } from "../models/RoomStrategy.js";
 
 class RoomService {
-    constructor(roomRepository, roomTypeRepository) {
-    this.roomRepository     = roomRepository
-    this.roomTypeRepository = roomTypeRepository
-    }
+  constructor(roomRepository, roomTypeRepository) {
+    this.roomRepository = roomRepository;
+    this.roomTypeRepository = roomTypeRepository;
+  }
 
-    getAll() {
-    return this.roomRepository.findAll()
-    }
+  getAll() {
+    return this.roomRepository.findAll();
+  }
 
-    getRoomTypes() {
-    return this.roomTypeRepository.findAll()
-    }
+  getRoomTypes() {
+    return this.roomTypeRepository.findAll();
+  }
 
-    applyStrategy(type) {
-    const roomType = this.roomTypeRepository.findByType(type)
-    if (!roomType) {
-        throw new Error(`Tipo no valido: ${type}`)
-    }
-    const strategy = getRoomStrategy(roomType)
-    return strategy.toJSON()
-    }
+  applyStrategy(type) {
+    const roomType = this.roomTypeRepository.findByType(type);
+    if (!roomType) throw new Error(`Tipo no valido: ${type}`);
+    const strategy = getRoomStrategy(roomType);
+    return strategy.toJSON();
+  }
 
-    availableRooms(checkIn, checkOut, type = null) {
+  availableRooms(checkIn, checkOut, type = null) {
+    this._validateDateRange(checkIn, checkOut);
     if (type) {
-        const roomType = this.roomTypeRepository.findByType(type)
-        if (!roomType) throw new Error(`Tipo no valido: ${type}`)
-        getRoomStrategy(roomType).validate()
+      const roomType = this.roomTypeRepository.findByType(type);
+      if (!roomType) throw new Error(`Tipo no valido: ${type}`);
+      getRoomStrategy(roomType).validate();
     }
-    const rooms = this.roomRepository.findAvailable(checkIn, checkOut, type)
-    if (!rooms || rooms.length === 0) {
-        throw new Error('No hay habitaciones disponibles en esas fechas')
-    }
-    return rooms
-    }
+    const rooms = this.roomRepository.findAvailable(checkIn, checkOut, type);
+    if (!rooms || rooms.length === 0)
+      throw new Error("No hay habitaciones disponibles en esas fechas");
+    return rooms;
+  }
+
+  _validateDateRange(checkIn, checkOut) {
+    if (checkIn >= checkOut)
+      throw new Error("Check-in debe ser anterior a check-out");
+  }
 }
 
-export default RoomService
+export default RoomService;
