94036c2683dfac407af4a0c8ee3bd53ddc8348b3
linuxenthusiastic <mbitakrec@gmail.com>
2026-06-19 22:48:22 -0400

    feat: [HU-04] implementar validación de fecha de checkin en CheckinService

--- src/services/CheckinService.js
+++ src/services/CheckinService.js
@@ -1,19 +1,30 @@
 import { getReservationState } from "../models/ReservationState.js";
 
 class CheckinService {
-  constructor(CheckinRepository, ReservationRepository) {
-    this.CheckinRepository = CheckinRepository;
-    this.ReservationRepository = ReservationRepository;
+  constructor(checkinRepository, reservationRepository) {
+    this.checkinRepository = checkinRepository;
+    this.reservationRepository = reservationRepository;
   }
+
   checkin(reservationId) {
-    const reservation = this.ReservationRepository.findById(reservationId);
+    const reservation = this.reservationRepository.findById(reservationId);
     if (!reservation) throw new Error("No existe la reservacion");
+
+    this._validateCheckinDate(reservation.check_in_date);
 
     const state = getReservationState(reservation.status);
     state.checkin();
 
-    this.CheckinRepository.saveCheckin(reservationId);
-    return this.ReservationRepository.updateStatus(reservationId, "CHECKED_IN");
+    this.checkinRepository.saveCheckin(reservationId);
+    return this.reservationRepository.updateStatus(reservationId, "CHECKED_IN");
+  }
+
+  _validateCheckinDate(checkInDate) {
+    const today = new Date().toISOString().split("T")[0];
+    if (checkInDate > today)
+      throw new Error(
+        `No se puede hacer checkin antes de la fecha programada: ${checkInDate}`
+      );
   }
 }
 
